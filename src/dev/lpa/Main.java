dafc1030bf07c9ff528952bbc82044ab71ccbee9
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-09-18 14:43:27 +0200

    run different colored countdown threads

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -6,7 +6,14 @@ public class Main {
 
   public static void main(String[] args) {
 
-
+    StopWatch stopWatch = new StopWatch(TimeUnit.SECONDS);
+    Thread green = new Thread(stopWatch::countDown, ThreadColor.ANSI_GREEN.name());
+    Thread purple = new Thread(() -> stopWatch.countDown(7),
+      ThreadColor.ANSI_PURPLE.name());
+    Thread red = new Thread(stopWatch::countDown, ThreadColor.ANSI_RED.name());
+    green.start();
+    purple.start();
+    red.start();
   }
 }
 
